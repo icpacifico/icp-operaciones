903c8137117c3625d7f47b30833f35f2d0ac37b4
icpacifico <cordillerainmobiliaria.redes@gmail.com>
2022-04-06 10:46:24 -0400

    commit en nuevo branch

--- padmin_ope/class/conexion.php
+++ padmin_ope/class/conexion.php
@@ -13,16 +13,16 @@ class conexion
 	
 	//-------PARAMETROS DE CONEXION
 	function __construct(){
-		$this->servidor = "localhost";
-		$this->usuario = "root";
-		$this->pass = "Proyectarse2022!!";
-		$this->base_datos = "ppsavcl_ssoopp_digital";
-        
-		// DESARROLLO
 		// $this->servidor = "localhost";
 		// $this->usuario = "root";
-		// $this->pass = "";
+		// $this->pass = "Proyectarse2022!!";
 		// $this->base_datos = "ppsavcl_ssoopp_digital";
+        
+		// DESARROLLO
+		$this->servidor = "localhost";
+		$this->usuario = "root";
+		$this->pass = "";
+		$this->base_datos = "ppsavcl_ssoopp_digital";
 		$this->conectar_base_datos();
 	}
 	//-------CONEXION A UNA BASE DE DATOS
