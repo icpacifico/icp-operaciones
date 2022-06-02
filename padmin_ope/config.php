43e7595302f830d5f39b137e907a2bf0a5752148
icpacifico <93926262+icpacifico@users.noreply.github.com>
2022-06-02 13:31:02 -0400

    Update config.php

--- padmin_ope/config.php
+++ padmin_ope/config.php
@@ -18,7 +18,7 @@ require "parametros.php";
 if (!defined('_RUTA')) {
 	// PRODUCCIÓN
 	if($PROD){
-	  define('_RUTA', "https://".$inicioruta."00ppsav.cl/");
+	  define('_RUTA', "https://".$inicioruta."test.00ppsav.cl/");
 	}else{
 	  //DESARROLLO
 	  define('_RUTA', "http://localhost/icp-operaciones/");
