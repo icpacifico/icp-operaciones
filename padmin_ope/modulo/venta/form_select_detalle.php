1d5f43506b4a57005ba635981efbebb174ffa9f4
icpacifico <cordillerainmobiliaria.redes@gmail.com>
2022-09-12 13:22:47 -0300

    diseño de sweet alert

--- padmin_ope/modulo/venta/form_select_detalle.php
+++ padmin_ope/modulo/venta/form_select_detalle.php
@@ -9,8 +9,35 @@ if (!isset($_SESSION["modulo_venta_panel"])) {
     header("Location: "._ADMIN."panel.php");
 }
 $_SESSION["id_venta_panel"] = $_GET["id"];
-require_once _INCLUDE."head.php";
+
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Plataforma Online</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- <link rel="stylesheet" href="<?php echo _ASSETS?>bootstrap5/css/bootstrap.min.css">
+  <script src="<?php echo _ASSETS?>bootstrap5/js/bootstrap.bundle.min.js"></script> -->
+
+
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="<?php echo _ASSETS?>bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo _ASSETS?>font-awesome-4.7.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo _ASSETS?>dist/css/AdminLTE.min.css">
+
+  <!-- <link rel="stylesheet" href="<?php echo _ASSETS?>plugins/alert/sweet-alert.css">  -->
+  <link rel="stylesheet" href="<?php echo _ASSETS?>dist/css/skins/_all-skins.min.css">  
+  <link rel="stylesheet" href="<?php echo _ASSETS?>plugins/alert_prueba/dist/sweetalert.css">
+  <link rel="stylesheet" href="<?php echo _ASSETS?>dist/css/ajustes.css">
+  <script src="<?php echo _ASSETS?>plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="<?php echo _ASSETS?>plugins/jQueryUI/jquery-ui.min.js"></script>
 <!-- DataTables -->
 <link rel="stylesheet" type="text/css" href="<?php echo _ASSETS?>plugins/datatables/dataTables.bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="<?php echo _ASSETS?>plugins/datatables/extensions/buttons/buttons.bootstrap.min.css">
