93ca1254f46dde756e1ac920b2059dd5ec730cbc
osman <osman.ahumada@gmail.com>
2022-09-25 22:14:02 -0300

    solucion a asignación de cliente

--- padmin_ope/modulo/evaluacion/asignacionCliente.php
+++ padmin_ope/modulo/evaluacion/asignacionCliente.php
@@ -9,29 +9,26 @@ include _INCLUDE."class/conexion.php";
 $conexion = new conexion();
 $data = conexion::select("SELECT rut_pro,id_pro FROM propietario_propietario");
 $count = 0;
-// echo '<ul>';
 try {
-    foreach($data as $val){   
-        // echo '<li style="border:1px solid black;padding:3px;width:150px;">cliente : '.$val['nombre_pro'].'</li>';
+    foreach($data as $val){           
         $data2 = conexion::select("SELECT id_cot,id_vend FROM cotizacion_cotizacion WHERE id_pro =".$val['id_pro']." GROUP BY id_cot DESC LIMIT 0,1");
-            if(count($data2)>0){
-                // echo '<ul>';
-                foreach($data2 as $var){
-                    // echo '<li>N° coti. '.$var['id_cot'].' -> vendedor n° '.$var['id_vend'].'</li>';
-                    $query = "INSERT INTO vendedor_propietario_vendedor(id_vend,id_pro)VALUES(?,?)";
-                    $conexion->consulta_form($query,array($var['id_vend'],$val['id_pro']));
+            if(count($data2)>0){                
+                foreach($data2 as $var){   
+                    $delete = "DELETE FROM vendedor_propietario_vendedor WHERE id_pro=?";                
+                    $insert = "INSERT INTO vendedor_propietario_vendedor(id_vend,id_pro)VALUES(?,?)";
+                    $conexion->consulta_form($delete,array($val['id_pro']));
+                    $conexion->consulta_form($insert,array($var['id_vend'],$val['id_pro']));
                     $count += 1;                  
-                }
-                // echo '</ul>';
+                }                
             }
         
     }
 } catch (\Throwable $th) {
     throw $th;
 } catch(Exception $e){
-    echo $e->message();
+    echo $e->getMessage();
 } catch (PDOException $e){
-    echo $e->message();
+    echo $e->getMessage();
 }
 echo 'Se han registrado '.$count.'enlaces en vendedor->propietario , congratuleishons';
 
